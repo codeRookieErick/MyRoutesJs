20351b986436a073c6dbdf417b69f3d2b79fa613
erick-commits <erickfernandomoraramirez@gmail.com>
2020-06-09 18:39:44 -0400

    Broken

--- main-server.js
+++
@@ -1,8 +0,0 @@
-const MyRouter = require("./lib/MyRouter");
-const MyModuleWatcher = require("./lib/MyModuleWatcher");
-
-const watcher = new  MyModuleWatcher();
-
-server = new MyRouter();
-watcher.watch("app", (app) => app(server));
-server.listen(8084);
